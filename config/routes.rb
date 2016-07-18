3e45a2f340b388889fcb51bb4274fafb1164d97c
Jason Ball <jasonlukeball@me.com>
2016-07-18 21:59:34 +1000

    Install & Configure Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+  devise_for :admins
+  resources :admins, only: [:show]
+
+  as :admin do
+    get 'admins', :to => 'admins#show', :as => :admin_root
+  end
 
 end
