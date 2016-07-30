23974975f87af694f15faa10e94aa538f90bc21b
Jason Ball <jasonlukeball@me.com>
2016-07-30 19:52:55 +1000

    Add User/Event Tracking JS

--- public/analytics.js
+++ public/analytics.js
@@ -1,23 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery-ui
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap
-//= require_tree .
-
-
 /* ==========================================================================
  USER & EVENT TRACKING
  ========================================================================== */
@@ -80,14 +60,3 @@ analytics.track = function(eventName){
 
 
  */
-
-
-/* ==========================================================================
- ALERT ANIMATION
- ========================================================================== */
-
-$('document').ready(function() {
-    setTimeout(function() {
-        $(".alert").slideUp(150);
-    }, 3000);
-});
